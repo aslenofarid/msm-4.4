LineageOS/android_kernel_asus_sdm660

input: synaptics_dsx_2.7: Import X00TD changes
* Extracted from S323 kernel source.
* Simplify syna_gesture_mode check in mdss.
* Changes to test reporting driver are ignored, because:
  1. Unnecessary; and
  2. who in the world will run SMMI test on custom ROM?

Signed-off-by: Albert I <krascgq@outlook.co.id>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

drivers/input/touchscreen/synaptics_dsx_2.7/synaptics_dsx_active_pen.c
@@ -137,6 +137,10 @@ static void apen_lift(void)
 	input_report_key(apen->apen_dev, BTN_TOOL_RUBBER, 0);
 	input_sync(apen->apen_dev);
 	apen->apen_present = false;
+
+#ifdef CONFIG_MACH_ASUS_X00TD
+	return;
+#endif
 }
 
 static void apen_report(void)
@@ -233,6 +237,10 @@ static void apen_report(void)
 			x, y, pressure);
 
 	apen->apen_present = true;
+
+#ifdef CONFIG_MACH_ASUS_X00TD
+	return;
+#endif
 }
 
 static void apen_set_params(void)
@@ -368,6 +376,9 @@ static int apen_scan_pdt(void)
 				switch (fd.fn_number) {
 				case SYNAPTICS_RMI4_F12:
 					goto f12_found;
+#ifdef CONFIG_MACH_ASUS_X00TD
+					break;
+#endif
 				}
 			} else {
 				break;
@@ -534,6 +545,10 @@ static void synaptics_rmi4_apen_remove(struct synaptics_rmi4_data *rmi4_data)
 
 exit:
 	complete(&apen_remove_complete);
+
+#ifdef CONFIG_MACH_ASUS_X00TD
+	return;
+#endif
 }
 
 static void synaptics_rmi4_apen_reset(struct synaptics_rmi4_data *rmi4_data)
@@ -546,6 +561,10 @@ static void synaptics_rmi4_apen_reset(struct synaptics_rmi4_data *rmi4_data)
 	apen_lift();
 
 	apen_scan_pdt();
+
+#ifdef CONFIG_MACH_ASUS_X00TD
+	return;
+#endif
 }
 
 static void synaptics_rmi4_apen_reinit(struct synaptics_rmi4_data *rmi4_data)
@@ -554,6 +573,10 @@ static void synaptics_rmi4_apen_reinit(struct synaptics_rmi4_data *rmi4_data)
 		return;
 
 	apen_lift();
+
+#ifdef CONFIG_MACH_ASUS_X00TD
+	return;
+#endif
 }
 
 static void synaptics_rmi4_apen_e_suspend(struct synaptics_rmi4_data *rmi4_data)
@@ -562,6 +585,10 @@ static void synaptics_rmi4_apen_e_suspend(struct synaptics_rmi4_data *rmi4_data)
 		return;
 
 	apen_lift();
+
+#ifdef CONFIG_MACH_ASUS_X00TD
+	return;
+#endif
 }
 
 static void synaptics_rmi4_apen_suspend(struct synaptics_rmi4_data *rmi4_data)
@@ -570,6 +597,10 @@ static void synaptics_rmi4_apen_suspend(struct synaptics_rmi4_data *rmi4_data)
 		return;
 
 	apen_lift();
+
+#ifdef CONFIG_MACH_ASUS_X00TD
+	return;
+#endif
 }
 
 static struct synaptics_rmi4_exp_fn active_pen_module = {
@@ -597,6 +628,10 @@ static void __exit rmi4_active_pen_module_exit(void)
 	synaptics_rmi4_new_function(&active_pen_module, false);
 
 	wait_for_completion(&apen_remove_complete);
+
+#ifdef CONFIG_MACH_ASUS_X00TD
+	return;
+#endif
 }
 
 module_init(rmi4_active_pen_module_init);

drivers/input/touchscreen/synaptics_dsx_2.7/synaptics_dsx_core.h
@@ -38,6 +38,16 @@
 #define SYNAPTICS_DS5 (1 << 1)
 #define SYNAPTICS_DSX_DRIVER_PRODUCT (SYNAPTICS_DS4 | SYNAPTICS_DS5)
 #define SYNAPTICS_DSX_DRIVER_VERSION 0x2070
+#ifdef CONFIG_MACH_ASUS_X00TD
+#define SYNA_POWER_SOURCE_CUST_EN 1
+#endif
+
+#if SYNA_POWER_SOURCE_CUST_EN
+#define LCM_LAB_MIN_UV	6000000
+#define LCM_LAB_MAX_UV	6000000
+#define LCM_IBB_MIN_UV	6000000
+#define LCM_IBB_MAX_UV	6000000
+#endif
 
 #include <linux/version.h>
 #ifdef CONFIG_FB
@@ -398,7 +408,9 @@ struct synaptics_rmi4_data {
 	int sensor_max_y;
 	int force_min;
 	int force_max;
+#ifndef CONFIG_MACH_ASUS_X00TD
 	int set_wakeup_gesture;
+#endif
 	bool flash_prog_mode;
 	bool irq_enabled;
 	bool fingers_on_2d;
@@ -422,6 +434,12 @@ struct synaptics_rmi4_data {
 			bool enable);
 	void (*report_touch)(struct synaptics_rmi4_data *rmi4_data,
 			struct synaptics_rmi4_fn *fhandler);
+#if SYNA_POWER_SOURCE_CUST_EN
+	struct regulator *lcm_lab;
+	struct regulator *lcm_ibb;
+	atomic_t lcm_lab_power;
+	atomic_t lcm_ibb_power;
+#endif
 };
 
 struct synaptics_dsx_bus_access {

drivers/input/touchscreen/synaptics_dsx_2.7/synaptics_dsx_fw_update.c
@@ -43,10 +43,13 @@
 #include "synaptics_dsx_core.h"
 
 #define FW_IHEX_NAME "synaptics/startup_fw_update.bin"
-#define FW_IMAGE_NAME "synaptics/startup_fw_update.img"
-/*
+#ifdef CONFIG_MACH_ASUS_X00TD
+#define FW_IMAGE_NAME "startup_fw_update.img"
 #define DO_STARTUP_FW_UPDATE
-*/
+#else
+#define FW_IMAGE_NAME "synaptics/startup_fw_update.img"
+#endif
+
 /*
 #ifdef DO_STARTUP_FW_UPDATE
 #ifdef CONFIG_FB
@@ -60,7 +63,11 @@
 #define MAX_WRITE_SIZE 4096
 */
 
+#ifdef CONFIG_MACH_ASUS_X00TD
+#define ENABLE_SYS_REFLASH true
+#else
 #define ENABLE_SYS_REFLASH false
+#endif
 #define FORCE_UPDATE false
 #define DO_LOCKDOWN false
 
@@ -3953,7 +3960,9 @@ int set_tddi_lockdown_data(unsigned char *lockdown_data, unsigned short leng)
 	if (retval < 0)
 		goto exit;
 
+#ifndef CONFIG_MACH_ASUS_X00TD
 	blk_cnt = fwu->blkcount.tddi_lockdown_data;
+#endif
 
 	fwu->config_size = fwu->blkcount.tddi_lockdown_data * fwu->block_size;
 	retval = fwu_allocate_read_config_buf(fwu->config_size);

aslenofarid/msm-4.4

drivers/input/touchscreen/synaptics_dsx_2.7/synaptics_dsx_active_pen.c
@@ -137,6 +137,10 @@ static void apen_lift(void)
 	input_report_key(apen->apen_dev, BTN_TOOL_RUBBER, 0);
 	input_sync(apen->apen_dev);
 	apen->apen_present = false;
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	return;
+#endif
 }
 
 static void apen_report(void)
@@ -233,6 +237,10 @@ static void apen_report(void)
 			x, y, pressure);
 
 	apen->apen_present = true;
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	return;
+#endif
 }
 
 static void apen_set_params(void)
@@ -368,6 +376,9 @@ static int apen_scan_pdt(void)
 				switch (fd.fn_number) {
 				case SYNAPTICS_RMI4_F12:
 					goto f12_found;
+#ifdef CONFIG_MACH_ASUS_X00T
+					break;
+#endif
 				}
 			} else {
 				break;
@@ -534,6 +545,10 @@ static void synaptics_rmi4_apen_remove(struct synaptics_rmi4_data *rmi4_data)
 
 exit:
 	complete(&apen_remove_complete);
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	return;
+#endif
 }
 
 static void synaptics_rmi4_apen_reset(struct synaptics_rmi4_data *rmi4_data)
@@ -546,6 +561,10 @@ static void synaptics_rmi4_apen_reset(struct synaptics_rmi4_data *rmi4_data)
 	apen_lift();
 
 	apen_scan_pdt();
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	return;
+#endif
 }
 
 static void synaptics_rmi4_apen_reinit(struct synaptics_rmi4_data *rmi4_data)
@@ -554,6 +573,10 @@ static void synaptics_rmi4_apen_reinit(struct synaptics_rmi4_data *rmi4_data)
 		return;
 
 	apen_lift();
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	return;
+#endif
 }
 
 static void synaptics_rmi4_apen_e_suspend(struct synaptics_rmi4_data *rmi4_data)
@@ -562,6 +585,10 @@ static void synaptics_rmi4_apen_e_suspend(struct synaptics_rmi4_data *rmi4_data)
 		return;
 
 	apen_lift();
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	return;
+#endif
 }
 
 static void synaptics_rmi4_apen_suspend(struct synaptics_rmi4_data *rmi4_data)
@@ -570,6 +597,10 @@ static void synaptics_rmi4_apen_suspend(struct synaptics_rmi4_data *rmi4_data)
 		return;
 
 	apen_lift();
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	return;
+#endif
 }
 
 static struct synaptics_rmi4_exp_fn active_pen_module = {
@@ -597,6 +628,10 @@ static void __exit rmi4_active_pen_module_exit(void)
 	synaptics_rmi4_new_function(&active_pen_module, false);
 
 	wait_for_completion(&apen_remove_complete);
+
+#ifdef CONFIG_MACH_ASUS_X00T
+	return;
+#endif
 }
 
 module_init(rmi4_active_pen_module_init);

drivers/input/touchscreen/synaptics_dsx_2.7/synaptics_dsx_core.h
@@ -38,6 +38,16 @@
 #define SYNAPTICS_DS5 (1 << 1)
 #define SYNAPTICS_DSX_DRIVER_PRODUCT (SYNAPTICS_DS4 | SYNAPTICS_DS5)
 #define SYNAPTICS_DSX_DRIVER_VERSION 0x2070
+#ifdef CONFIG_MACH_ASUS_X00T
+#define SYNA_POWER_SOURCE_CUST_EN 1
+#endif
+
+#if SYNA_POWER_SOURCE_CUST_EN
+#define LCM_LAB_MIN_UV	6000000
+#define LCM_LAB_MAX_UV	6000000
+#define LCM_IBB_MIN_UV	6000000
+#define LCM_IBB_MAX_UV	6000000
+#endif
 
 #include <linux/version.h>
 #ifdef CONFIG_FB
@@ -398,7 +408,9 @@ struct synaptics_rmi4_data {
 	int sensor_max_y;
 	int force_min;
 	int force_max;
+#ifndef CONFIG_MACH_ASUS_X00T
 	int set_wakeup_gesture;
+#endif
 	bool flash_prog_mode;
 	bool irq_enabled;
 	bool fingers_on_2d;
@@ -422,6 +434,12 @@ struct synaptics_rmi4_data {
 			bool enable);
 	void (*report_touch)(struct synaptics_rmi4_data *rmi4_data,
 			struct synaptics_rmi4_fn *fhandler);
+#if SYNA_POWER_SOURCE_CUST_EN
+	struct regulator *lcm_lab;
+	struct regulator *lcm_ibb;
+	atomic_t lcm_lab_power;
+	atomic_t lcm_ibb_power;
+#endif
 };
 
 struct synaptics_dsx_bus_access {

drivers/input/touchscreen/synaptics_dsx_2.7/synaptics_dsx_fw_update.c
@@ -43,10 +43,13 @@
 #include "synaptics_dsx_core.h"
 
 #define FW_IHEX_NAME "synaptics/startup_fw_update.bin"
-#define FW_IMAGE_NAME "synaptics/startup_fw_update.img"
-/*
+#ifdef CONFIG_MACH_ASUS_X00T
+#define FW_IMAGE_NAME "startup_fw_update.img"
 #define DO_STARTUP_FW_UPDATE
-*/
+#else
+#define FW_IMAGE_NAME "synaptics/startup_fw_update.img"
+#endif
+
 /*
 #ifdef DO_STARTUP_FW_UPDATE
 #ifdef CONFIG_FB
@@ -60,7 +63,11 @@
 #define MAX_WRITE_SIZE 4096
 */
 
+#ifdef CONFIG_MACH_ASUS_X00T
+#define ENABLE_SYS_REFLASH true
+#else
 #define ENABLE_SYS_REFLASH false
+#endif
 #define FORCE_UPDATE false
 #define DO_LOCKDOWN false
 
@@ -3953,7 +3960,9 @@ int set_tddi_lockdown_data(unsigned char *lockdown_data, unsigned short leng)
 	if (retval < 0)
 		goto exit;
 
+#ifndef CONFIG_MACH_ASUS_X00T
 	blk_cnt = fwu->blkcount.tddi_lockdown_data;
+#endif
 
 	fwu->config_size = fwu->blkcount.tddi_lockdown_data * fwu->block_size;
 	retval = fwu_allocate_read_config_buf(fwu->config_size);